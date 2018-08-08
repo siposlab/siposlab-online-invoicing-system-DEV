95565d01270bf5c453cb6bbb08b288676dc88308
Scapola <scapola.andrea@gmail.com>
2018-08-08 16:47:01 +0200

    Fixed 13197
    Vulnerabilities (Critical)

--- app/admin/toolTips.js
+++ app/admin/toolTips.js
@@ -1,13 +1,13 @@
-<!-- 
+<!--
 /*
  Pleas leave this notice.
  DHTML tip message version 1.2 copyright Essam Gamal 2003 (http://migoicons.tripod.com, migoicons@hotmail.com)
  All modifications are done in the style.js you should not modify this file.  Created on : 06/03/2003
  Script featured on and can be found at Dynamic Drive (http://www.dynamicdrive.com)
-*/ 
+*/
 
 var ua = navigator.userAgent
-var ps = navigator.productSub 
+var ps = navigator.productSub
 var dom = (document.getElementById)? 1:0
 var ie4 = (document.all&&!dom)? 1:0
 var ie5 = (document.all&&dom)? 1:0
@@ -18,18 +18,18 @@ var cssFilters = ((ua.indexOf("MSIE 5.5")>=0||ua.indexOf("MSIE 6")>=0)&&ua.index
 var Style=[],Text=[],Count=0,sbw=0,move=0,hs="",mx,my,scl,sct,ww,wh,obj,sl,st,ih,iw,vl,hl,sv,evlh,evlw,tbody
 var HideTip = "eval(obj+sv+hl+';'+obj+sl+'=0;'+obj+st+'=-800')"
 var doc_root = ((ie5&&ua.indexOf("Opera")<0||ie4)&&document.compatMode=="CSS1Compat")? "document.documentElement":"document.body"
-var PX = (nn6)? "px" :"" 
+var PX = (nn6)? "px" :""
 
 if(sNav) {
 	window.onresize = ReloadTip
 	document.onmousemove = MoveTip
-	if(nn4) document.captureEvents(Event.MOUSEMOVE) 
-}	
+	if(nn4) document.captureEvents(Event.MOUSEMOVE)
+}
 if(nn4||nn6) {
 	mx = "e.pageX"
 	my = "e.pageY"
 	scl = "window.pageXOffset"
-	sct = "window.pageYOffset"	
+	sct = "window.pageYOffset"
 	if(nn4) {
 		obj = "document.TipLayer."
 		sl = "left"
@@ -41,7 +41,7 @@ if(nn4||nn6) {
 		sv = "visibility="
 	}
 	else obj = "document.getElementById('TipLayer')."
-} 
+}
 if(ie4||ie5) {
 	obj = "TipLayer."
 	mx = "event.x"
@@ -49,7 +49,7 @@ if(ie4||ie5) {
 	scl = "eval(doc_root).scrollLeft"
 	sct = "eval(doc_root).scrollTop"
 	if(ie5) {
-		mx = mx+"+"+scl 
+		mx = mx+"+"+scl
 		my = my+"+"+sct
 	}
 }
@@ -65,17 +65,16 @@ if(ie4||dom){
 if(ie4||ie5||ps>=20020823) {
 	ww = "eval(doc_root).clientWidth"
 	wh = "eval(doc_root).clientHeight"
-}	 
-else { 
+}
+else {
 	ww = "window.innerWidth"
 	wh = "window.innerHeight"
 	evlh = (wh)
-	evlw = eval(ww)
 	sbw=15
-}	
+}
 
 function applyCssFilter(){
-	if(cssFilters&&FiltersEnabled) { 
+	if(cssFilters&&FiltersEnabled) {
 		var dx = " progid:DXImageTransform.Microsoft."
 		TipLayer.style.filter = "revealTrans()"+dx+"Fade(Overlap=1.00 enabled=0)"+dx+"Inset(enabled=0)"+dx+"Iris(irisstyle=PLUS,motion=in enabled=0)"+dx+"Iris(irisstyle=PLUS,motion=out enabled=0)"+dx+"Iris(irisstyle=DIAMOND,motion=in enabled=0)"+dx+"Iris(irisstyle=DIAMOND,motion=out enabled=0)"+dx+"Iris(irisstyle=CROSS,motion=in enabled=0)"+dx+"Iris(irisstyle=CROSS,motion=out enabled=0)"+dx+"Iris(irisstyle=STAR,motion=in enabled=0)"+dx+"Iris(irisstyle=STAR,motion=out enabled=0)"+dx+"RadialWipe(wipestyle=CLOCK enabled=0)"+dx+"RadialWipe(wipestyle=WEDGE enabled=0)"+dx+"RadialWipe(wipestyle=RADIAL enabled=0)"+dx+"Pixelate(MaxSquare=35,enabled=0)"+dx+"Slide(slidestyle=HIDE,Bands=25 enabled=0)"+dx+"Slide(slidestyle=PUSH,Bands=25 enabled=0)"+dx+"Slide(slidestyle=SWAP,Bands=25 enabled=0)"+dx+"Spiral(GridSizeX=16,GridSizeY=16 enabled=0)"+dx+"Stretch(stretchstyle=HIDE enabled=0)"+dx+"Stretch(stretchstyle=PUSH enabled=0)"+dx+"Stretch(stretchstyle=SPIN enabled=0)"+dx+"Wheel(spokes=16 enabled=0)"+dx+"GradientWipe(GradientSize=1.00,wipestyle=0,motion=forward enabled=0)"+dx+"GradientWipe(GradientSize=1.00,wipestyle=0,motion=reverse enabled=0)"+dx+"GradientWipe(GradientSize=1.00,wipestyle=1,motion=forward enabled=0)"+dx+"GradientWipe(GradientSize=1.00,wipestyle=1,motion=reverse enabled=0)"+dx+"Zigzag(GridSizeX=8,GridSizeY=8 enabled=0)"+dx+"Alpha(enabled=0)"+dx+"Dropshadow(OffX=3,OffY=3,Positive=true,enabled=0)"+dx+"Shadow(strength=3,direction=135,enabled=0)"
 	}
@@ -95,13 +94,13 @@ function stm(t,s) {
 		var txtCol = (s[1])? "COLOR='"+s[1]+"'" : ""
 		var titBgCol = (s[2])? "BGCOLOR='"+s[2]+"'" : ""
 		var txtBgCol = (s[3])? "BGCOLOR='"+s[3]+"'" : ""
-		var titBgImg = (s[4])? "BACKGROUND='"+s[4]+"'" : ""	
+		var titBgImg = (s[4])? "BACKGROUND='"+s[4]+"'" : ""
 		var txtBgImg = (s[5])? "BACKGROUND='"+s[5]+"'" : ""
 		var titTxtAli = (s[6] && s[6].toLowerCase()!="left")? "ALIGN='"+s[6]+"'" : ""
-		var txtTxtAli = (s[7] && s[7].toLowerCase()!="left")? "ALIGN='"+s[7]+"'" : ""   
+		var txtTxtAli = (s[7] && s[7].toLowerCase()!="left")? "ALIGN='"+s[7]+"'" : ""
 		var add_height = (s[15])? "HEIGHT='"+s[15]+"'" : ""
 		if(!s[8])  s[8] = "Verdana,Arial,Helvetica"
-		if(!s[9])  s[9] = "Verdana,Arial,Helvetica"					
+		if(!s[9])  s[9] = "Verdana,Arial,Helvetica"
 		if(!s[12]) s[12] = 1
 		if(!s[13]) s[13] = 1
 		if(!s[14]) s[14] = 200
@@ -109,7 +108,7 @@ function stm(t,s) {
 		if(!s[17]) s[17] = 0
 		if(!s[18]) s[18] = 10
 		if(!s[19]) s[19] = 10
-		hs = s[11].toLowerCase() 
+		hs = s[11].toLowerCase()
 		if(ps==20001108){
 		if(s[2]) ab="STYLE='border:"+s[16]+"px solid"+" "+s[2]+"'"
 		ap="STYLE='padding:"+s[17]+"px "+s[17]+"px "+s[17]+"px "+s[17]+"px'"}
@@ -125,21 +124,21 @@ function stm(t,s) {
 		}
 		else eval(obj+"innerHTML=txt")
 		tbody = {
-			Pos:s[10].toLowerCase(), 
+			Pos:s[10].toLowerCase(),
 			Xpos:s[18],
-			Ypos:s[19], 
+			Ypos:s[19],
 			Transition:s[20],
-			Duration:s[21], 
+			Duration:s[21],
 			Alpha:s[22],
 			ShadowType:s[23].toLowerCase(),
 			ShadowColor:s[24],
 			Width:parseInt(eval(obj+iw)+3+sbw)
 		}
-		if(ie4) { 
+		if(ie4) {
 			TipLayer.style.width = s[14]
 	 		tbody.Width = s[14]
 		}
-		Count=0	
+		Count=0
 		move=1
  	 }
   }
@@ -152,8 +151,8 @@ function MoveTip(e) {
 		switch(tbody.Pos) {
 			case "left" : X=MouseX-tbody.Width-tbody.Xpos; Y=MouseY+tbody.Ypos; break
 			case "center": X=MouseX-(tbody.Width/2); Y=MouseY+tbody.Ypos; break
-			case "float": X=tbody.Xpos+eval(scl); Y=tbody.Ypos+eval(sct); break	
-			case "fixed": X=tbody.Xpos; Y=tbody.Ypos; break		
+			case "float": X=tbody.Xpos+eval(scl); Y=tbody.Ypos+eval(sct); break
+			case "fixed": X=tbody.Xpos; Y=tbody.Ypos; break
 			default: X=MouseX+tbody.Xpos; Y=MouseY+tbody.Ypos
 		}
 
@@ -162,7 +161,7 @@ function MoveTip(e) {
 			if(tbody.Pos=="float"||tbody.Pos=="fixed") Y = tbody.wih-tbody.Height-sbw
 			else Y = MouseY-tbody.Height
 		}
-		if(X<0) X=0 
+		if(X<0) X=0
 		eval(obj+sl+"=X+PX;"+obj+st+"=Y+PX")
 		ViewTip()
 	}
@@ -171,13 +170,13 @@ function MoveTip(e) {
 function ViewTip() {
   	Count++
 	if(Count == 1) {
-		if(cssFilters&&FiltersEnabled) {	
+		if(cssFilters&&FiltersEnabled) {
 			for(Index=28; Index<31; Index++) { TipLayer.filters[Index].enabled = 0 }
 			for(s=0; s<28; s++) { if(TipLayer.filters[s].status == 2) TipLayer.filters[s].stop() }
 			if(tbody.Transition == 51) tbody.Transition = parseInt(Math.random()*50)
 			var applyTrans = (tbody.Transition>-1&&tbody.Transition<24&&tbody.Duration>0)? 1:0
 			var advFilters = (tbody.Transition>23&&tbody.Transition<51&&tbody.Duration>0)? 1:0
-			var which = (applyTrans)?0:(advFilters)? tbody.Transition-23:0 
+			var which = (applyTrans)?0:(advFilters)? tbody.Transition-23:0
 			if(tbody.Alpha>0&&tbody.Alpha<100) {
 	  			TipLayer.filters[28].enabled = 1
 	  			TipLayer.filters[28].opacity = tbody.Alpha
@@ -193,7 +192,7 @@ function ViewTip() {
 			if(applyTrans||advFilters) {
 				eval(obj+sv+hl)
 	  			if(applyTrans) TipLayer.filters[0].transition = tbody.Transition
-	  			TipLayer.filters[which].duration = tbody.Duration 
+	  			TipLayer.filters[which].duration = tbody.Duration
 	  			TipLayer.filters[which].apply()
 			}
 		}
@@ -215,13 +214,11 @@ function ReloadTip() {
 function htm() {
 	if(sNav) {
 		if(hs!="keep") {
-			move=0; 
+			move=0;
 			if(hs!="sticky") eval(HideTip)
-		}	
-	} 
+		}
+	}
 }
 
 
 //-->
-
-
