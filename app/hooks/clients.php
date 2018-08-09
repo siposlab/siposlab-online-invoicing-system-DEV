71bfbe07d244c35b4fc81a5580d1490322c696a3
Scapola <scapola.andrea@gmail.com>
2018-08-09 13:44:34 +0200

    resolved 13234 13232
    Code smell (Major)

--- app/hooks/clients.php
+++ app/hooks/clients.php
@@ -1,21 +1,16 @@
 <?php
 	// For help on using hooks, please refer to http://bigprof.com/appgini/help/working-with-generated-web-database-application/hooks
 
-	function clients_init(&$options, &$args){
+	function clients_init(&$options){
 
 		return TRUE;
 	}
 
-	function clients_header($contentType, &$args){
+	function clients_header($contentType){
 		$header='';
 
 		switch($contentType){
 			case 'tableview':
-				$header='';
-				break;
-
-
-			case 'tableview+detailview':
 				$header='';
 				break;
 
@@ -39,7 +34,7 @@
 		return $header;
 	}
 
-	function clients_footer($contentType, &$args){
+	function clients_footer($contentType){
 		$footer='';
 
 		switch($contentType){
@@ -74,40 +69,40 @@
 		return $footer;
 	}
 
-	function clients_before_insert(&$data, &$args){
+	function clients_before_insert(&$data){
 
 		return TRUE;
 	}
 
-	function clients_after_insert($data, &$args){
+	function clients_after_insert($data){
 
 		return TRUE;
 	}
 
-	function clients_before_update(&$data, &$args){
+	function clients_before_update(&$data){
 
 		return TRUE;
 	}
 
-	function clients_after_update($data, &$args){
+	function clients_after_update($data){
 
 		return TRUE;
 	}
 
-	function clients_before_delete($selectedID, &$skipChecks, &$args){
+	function clients_before_delete($selectedID, &$skipChecks){
 
 		return TRUE;
 	}
 
-	function clients_after_delete($selectedID, &$args){
+	function clients_after_delete($selectedID){
 
 	}
 
-	function clients_dv($selectedID, &$html, &$args){
+	function clients_dv($selectedID, &$html){
 
 	}
 
-	function clients_csv($query, $args){
+	function clients_csv($query){
 
 		return $query;
 	}
