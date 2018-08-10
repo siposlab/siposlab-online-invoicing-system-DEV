12f0d02c396dc9ec70a55064c89d60afdfbaa3e1
Francesco <francesco.signoriello95@gmail.com>
2018-08-10 16:59:20 +0200

    Resolved #13284

--- app/hooks/items.php
+++ app/hooks/items.php
@@ -1,12 +1,12 @@
 <?php
 	// For help on using hooks, please refer to http://bigprof.com/appgini/help/working-with-generated-web-database-application/hooks
 
-	function items_init(&$options, $memberInfo, &$args){
+	function items_init(){
 
 		return TRUE;
 	}
 
-	function items_header($contentType, $memberInfo, &$args){
+	function items_header($contentType){
 		$header='';
 
 		switch($contentType){
@@ -14,100 +14,37 @@
 				$header='';
 				break;
 
-			case 'detailview':
-				$header='';
-				break;
 
-			case 'tableview+detailview':
-				$header='';
-				break;
-
-			case 'print-tableview':
-				$header='';
-				break;
-
-			case 'print-detailview':
-				$header='';
-				break;
-
-			case 'filters':
-				$header='';
-				break;
+            default:
+                break;
 		}
 
 		return $header;
 	}
 
-	function items_footer($contentType, $memberInfo, &$args){
+	function items_footer($contentType){
 		$footer='';
 
 		switch($contentType){
-			case 'tableview':
-				$footer='';
-				break;
 
 			case 'detailview':
 				$footer='';
 				break;
 
-			case 'tableview+detailview':
-				$footer='';
-				break;
+            default:
+                break;
 
-			case 'print-tableview':
-				$footer='';
-				break;
-
-			case 'print-detailview':
-				$footer='';
-				break;
-
-			case 'filters':
-				$footer='';
-				break;
 		}
 
 		return $footer;
 	}
+	
 
-	function items_before_insert(&$data, $memberInfo, &$args){
-
-		return TRUE;
-	}
-
-	function items_after_insert($data, $memberInfo, &$args){
-
-		return TRUE;
-	}
-
-	function items_before_update(&$data, $memberInfo, &$args){
-
-		return TRUE;
-	}
-
-	function items_after_update($data, $memberInfo, &$args){
-
-		return TRUE;
-	}
-
-	function items_before_delete($selectedID, &$skipChecks, $memberInfo, &$args){
-
-		return TRUE;
-	}
-
-	function items_after_delete($selectedID, $memberInfo, &$args){
-
-	}
-
-	function items_dv($selectedID, $memberInfo, &$html, &$args){
-
-	}
-
-	function items_csv($query, $memberInfo, &$args){
+	function items_csv($query){
 
 		return $query;
 	}
-	function items_batch_actions(&$args){
+	function items_batch_actions(){
 
 		return array();
 	}
