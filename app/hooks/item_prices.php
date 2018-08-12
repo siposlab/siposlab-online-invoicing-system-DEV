80106d2fe55f773c19bbfcecbfdee7458aac56ba
Scapola <scapola.andrea@gmail.com>
2018-08-12 18:07:54 +0200

    Fixed 13282

--- app/hooks/item_prices.php
+++ app/hooks/item_prices.php
@@ -1,113 +1,53 @@
 <?php
 	// For help on using hooks, please refer to https://bigprof.com/appgini/help/working-with-generated-web-database-application/hooks
 
-	function item_prices_init(&$options, $memberInfo, &$args){
+	function item_prices_init(){
 
 		return TRUE;
 	}
 
-	function item_prices_header($contentType, $memberInfo, &$args){
-		$header='';
+	function item_prices_header(){
 
-		switch($contentType){
-			case 'tableview':
-				$header='';
-				break;
 
-			case 'detailview':
-				$header='';
-				break;
 
-			case 'tableview+detailview':
-				$header='';
-				break;
 
-			case 'print-tableview':
-				$header='';
-				break;
 
-			case 'print-detailview':
-				$header='';
-				break;
-
-			case 'filters':
-				$header='';
-				break;
-		}
-
-		return $header;
 	}
 
-	function item_prices_footer($contentType, $memberInfo, &$args){
-		$footer='';
+	function item_prices_footer(){
 
-		switch($contentType){
-			case 'tableview':
-				$footer='';
-				break;
 
-			case 'detailview':
-				$footer='';
-				break;
 
-			case 'tableview+detailview':
-				$footer='';
-				break;
 
-			case 'print-tableview':
-				$footer='';
-				break;
 
-			case 'print-detailview':
-				$footer='';
-				break;
-
-			case 'filters':
-				$footer='';
-				break;
-		}
-
-		return $footer;
 	}
 
-	function item_prices_before_insert(&$data, $memberInfo, &$args){
 
-		return TRUE;
-	}
-
-	function item_prices_after_insert($data, $memberInfo, &$args){
+	function item_prices_after_insert($data){
 		update_item_latest_price($data['item']);
 		return TRUE;
 	}
 
-	function item_prices_before_update(&$data, $memberInfo, &$args){
 
-		return TRUE;
-	}
 
-	function item_prices_after_update($data, $memberInfo, &$args){
-		update_item_latest_price($data['item']);
-		return TRUE;
-	}
-
-	function item_prices_before_delete($selectedID, &$skipChecks, $memberInfo, &$args){
+	function item_prices_before_delete($selectedID){
 		$GLOBALS['deleted_item_id'] = sqlValue("select item from item_prices where id='{$selectedID}'");
 		return TRUE;
 	}
 
-	function item_prices_after_delete($selectedID, $memberInfo, &$args){
+	function item_prices_after_delete(){
 		update_item_latest_price($GLOBALS['deleted_item_id']);
 	}
 
-	function item_prices_dv($selectedID, $memberInfo, &$html, &$args){
+	function item_prices_dv(){
 
 	}
 
-	function item_prices_csv($query, $memberInfo, &$args){
+	function item_prices_csv($query){
 
 		return $query;
 	}
-	function item_prices_batch_actions(&$args){
+	function item_prices_batch_actions(){
 
 		return array();
 	}
